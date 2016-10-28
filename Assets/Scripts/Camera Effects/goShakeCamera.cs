cafe09ec96f14faa35b7a9c42552cd6dde70b6b6
Emiliandro Firmino <emiliandro@live.com>
2016-10-28 03:04:36 -0400

    ShakeCamera

---
+++ Assets/Scripts/Camera Effects/goShakeCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goShakeCamera : MonoBehaviour {
+
+    [Header("Qual o alvo?")]
+    public GameObject alvo;
+    public float velocidade;
+
+    [Header("Tempo de duração efeito")]
+    public float tempo_total;
+    private float valorAtual, tempo_limite;
+
+    [Header("Controle do efeito")]
+    public float minima;
+    public float maxima;
+    private GameObject camera;
+    private int escolha;
+
+    void Start(){
+        Time.timeScale = 2f;
+        tempo_limite = tempo_total;
+        tempo_total = 0;
+    }
+
+    void shakeEffect() {
+        camera = GameObject.FindGameObjectWithTag("MainCamera");
+        escolha = Random.RandomRange(0, 4);
+        if (escolha == 0) camera.transform.position = new Vector2(camera.transform.position.x + Random.RandomRange(minima, maxima), camera.transform.position.y);
+        if (escolha == 1) camera.transform.position = new Vector2(camera.transform.position.x, camera.transform.position.y + Random.RandomRange(minima, maxima));
+        if (escolha == 2) camera.transform.position = new Vector2(camera.transform.position.x - Random.RandomRange(minima, maxima), camera.transform.position.y);
+        if (escolha == 3) camera.transform.position = new Vector2(camera.transform.position.x, camera.transform.position.y - Random.RandomRange(minima, maxima));
+    }
+
+    void Update () {
+        valorAtual += Time.deltaTime;
+        Debug.Log(valorAtual);
+        if (valorAtual < tempo_limite) shakeEffect();
+        if (valorAtual > tempo_limite) AnularEfeito();
+    }
+
+    void AnularEfeito() {
+        camera.transform.position = Vector2.MoveTowards(transform.position, alvo.transform.position, velocidade * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Particles/pingoNaTela.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pingoNaTela : MonoBehaviour {
+
+    [Header("Objeto a instanciar")]
+    public GameObject objeto;
+
+    [Header("Controle do efeito")]
+    public float minima;
+    public float maxima;
+
+
+    // Use this for initialization
+    void Start () {
+        Instantiate(objeto, new Vector2 (Random.RandomRange(minima, maxima), Random.RandomRange(minima, maxima)), Quaternion.identity);
+	
+	}
+	
+
+}
