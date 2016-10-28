10e8eb906c720d75461291b41c99d66a7e588385
Emiliandro Firmino <emiliandro@live.com>
2016-10-28 02:09:25 -0400

    Power Ups Effects and Image Effects added

---
+++ Assets/Scripts/Image Effects/FadeRed.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FadeRed : MonoBehaviour {
+
+    [Header("Indique a velocidade da ação")]
+    public float tempo;
+    private SpriteRenderer sprite;
+
+    void Start () {
+        sprite = gameObject.GetComponent<SpriteRenderer>();
+        goRed();
+    }
+
+    void goRed () {
+        sprite.color = Color.red;
+        StartCoroutine("anulaRed");
+
+    }
+
+    IEnumerator anulaRed() {
+        while (sprite.color.g < 0.99f && sprite.color.b < 0.99f)
+        {
+            sprite.color = new Color(sprite.color.r, sprite.color.g + (Time.deltaTime / tempo), sprite.color.b + (Time.deltaTime / tempo), sprite.color.a);
+            yield return new WaitForSeconds(Time.deltaTime / 2);
+        }
+    }
+
+}
---
+++ Assets/Scripts/PowerUps/destruirPU.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class destruirPU : MonoBehaviour {
+
+    [Header("Indique o tempo da ação")]
+    public float tempo;
+
+    void Start()
+    {
+        Invoke("anularEfeito", tempo);
+    }
+
+    void anularEfeito()
+    {
+        Destroy(this.gameObject);
+    }
+
+}
