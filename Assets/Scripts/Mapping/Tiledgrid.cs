7435d894e539bd2436d788926abf6a3d9a06b0d7
Emiliandro Firmino <emiliandro@live.com>
2016-10-31 02:10:43 -0400

    Tiled Grid

---
+++ Assets/Scripts/Mapping/Tiledgrid.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tiledgrid : MonoBehaviour {
+
+    [Header("Informações da imagem")]
+    public GameObject tiled;
+    private Transform posicao_inicial;
+    public int altura;
+    public int largura;
+
+    [Header("Informações do grid")]
+    public int blocos_de_altura;
+    public int blocos_de_largura;
+
+    void Start () {
+        posicao_inicial = gameObject.GetComponent<Transform>();
+        gridSetup();	
+	}
+
+	public void gridSetup () {
+        for (int i=0; i < blocos_de_largura; i++)
+        {
+            for (int u = 0; u < blocos_de_altura; u++)
+            {
+                Instantiate(tiled, new Vector2
+                    (posicao_inicial.position.x + (i * largura),
+                    posicao_inicial.position.y + (u * altura)),
+                    Quaternion.identity);
+            }
+        }
+	}
+}
